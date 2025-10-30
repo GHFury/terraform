9a110330ded5db1afad6bb17157022cec6cb151c
ghfury <brianjbarnwell@gmail.com>
2025-10-29 18:42:15 -0700

    chore: run terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,9 @@ module "iam" {
 }
 
 module "ecs" {
-  source                       = "../modules/ecs"
-  project_name                 = var.project_name
-  ecr_repo_url                 = module.ecr.repository_url
-  ecs_task_execution_role_arn  = module.iam.ecs_task_execution_role_arn
+  source                      = "../modules/ecs"
+  project_name                = var.project_name
+  ecr_repo_url                = module.ecr.repository_url
+  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
   # optional: pass image_tag, cpu, memory, subnet_ids, security_group_ids, desired_count
 }
