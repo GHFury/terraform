57b7ef41dc424a2bbccd9ee2cf2a8ccc6d0e9696
ghfury <brianjbarnwell@gmail.com>
2025-10-29 19:05:21 -0700

    ecr: move lifecycle policy to aws_ecr_lifecycle_policy resource (provider compatibility)

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -5,23 +5,25 @@ resource "aws_ecr_repository" "repo" {
     scan_on_push = true
   }
 
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
-        {
-          rulePriority = 1,
-          description  = "Keep last 10 images"
-          selection = {
-            tagStatus     = "any"
-            countType     = "imageCountMoreThan"
-            countNumber   = 10
-          }
-          action = {
-            type = "expire"
-          }
+
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.repo.name
+  policy     = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
         }
-      ]
-    })
-  }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
 }
 
